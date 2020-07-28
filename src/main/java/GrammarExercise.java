93652e64f956b972eeca6846901dffb09e753b48
Annie <anqi.chen@thoughtworks.com>
2020-07-28 21:01:55 +0800

    commit java-basic-exercise

--- src/main/java/GrammarExercise.java
+++ src/main/java/GrammarExercise.java
@@ -1,18 +1,44 @@
+import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class GrammarExercise {
     public static void main(String[] args) {
-        //需要从命令行读入
+
         String firstWordList = "";
         String secondWordList = "";
-
         List<String> result = findCommonWordsWithSpace(firstWordList,secondWordList);
-        //按要求输出到命令行
 
     }
 
     public static List<String> findCommonWordsWithSpace(String firstWordList, String secondWordList) {
         //在这编写实现代码
-        return null;
+        String[] firstArray = firstWordList.split(",");
+        String[] secondArray = secondWordList.split(",");
+        List<String> f1 = Arrays.asList(firstArray);
+        List<String> f2 = Arrays.asList(secondArray);
+        List<String> res11 = f1.stream().filter(str -> str.isEmpty()).collect(Collectors.toList());
+        List<String> res12 = f1.stream().filter(str -> str.matches(".*[^a-zA-Z]+.*")).collect(Collectors.toList());
+        List<String> res21 = f2.stream().filter(str -> str.isEmpty()).collect(Collectors.toList());
+        List<String> res22 = f2.stream().filter(str -> str.matches(".*[^a-zA-Z]+.*")).collect(Collectors.toList());
+
+
+        if (res11.size() + res12.size() + res21.size() + res22.size() > 0) {
+            throw new RuntimeException("input not valid");
+        }
+        List<String> f22 = f2.stream().map(s -> s.toUpperCase()).distinct().collect(Collectors.toList());
+        List<String> result = f1.stream().map(s -> s.toUpperCase()).filter(f22::contains).distinct().sorted().collect(Collectors.toList());
+        List<String> result1 = result.stream().map((s) -> {
+            String[] sar = s.split("");
+            String newstr="";
+            for(String st :sar){
+                newstr = newstr + " " + st;
+            }
+            return newstr.trim();
+        }).collect(Collectors.toList());
+
+
+        return result1;
     }
 }
